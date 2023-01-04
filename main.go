02b1c134080a25bd283e99321394f191794ad688
onishi taisei <taisei.developer@gmail.com>
2023-01-04 23:18:27 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func foo() {
+	xi := 1
+	var xf32 float32 = 1.2
+	xs := "test"
+	xt, xf := true, false
+	fmt.Println(xi, xf32, xs, xt, xf)
+	fmt.Printf("%T", xf32)
+	xs = "書き換えました"
+	fmt.Println(xi, xf32, xs, xt, xf)
+}
+
+func main() {
+	foo()
+
+	var (
+		i    int     = 1
+		f64  float64 = 1.2
+		s    string  = "test"
+		t, f bool    = true, false
+	)
+	fmt.Println(i, f64, s, t, f)
+
+}
